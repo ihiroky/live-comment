8a746fc32da46551f1277b71a07425565c065e73
Hiroki Itoh <ihiroky@gmail.com>
2020-09-12 19:12:13 +0900

    Use callback of WebSocket.send() to handle slow clients.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,17 +1,7 @@
 import fs from 'fs'
 import https from 'https'
-import WebSocket from 'ws'
 import 'tslib'
-import { v4 as uuidv4 } from 'uuid'
-
-interface MyWebSocket extends WebSocket {
-  alive?: boolean
-  id?: string
-  connectedTime?: number
-  lastPongTime?: number
-}
-
-const PING_INTERVAL_MILLIS = 7 * 1000
+import { createWebSocketServer } from './websocket'
 
 const server = https.createServer({
   cert: fs.readFileSync('dist/cert.pem'),
@@ -19,43 +9,6 @@ const server = https.createServer({
 }).on('request', (_, res): void => {
   res.end('Hello.')
 })
-
-function heartbeat(this: WebSocket, data: Buffer): void {
-  const self = this as MyWebSocket
-  console.log('heartbeat', self.id, data)
-  self.alive = true
-  self.lastPongTime = Date.now()
-}
-
-const wss = new WebSocket.Server({ server })
-wss.on('connection', function(ws) {
-  const client = ws as MyWebSocket
-  client.id = uuidv4()
-  client.alive = true
-  client.connectedTime = Date.now()
-  client.on('pong', heartbeat)
-  client.on('message', function(message) {
-    console.log('message', message)
-    wss.clients.forEach(c => c.send(message))
-  })
-  client.send('connected.')
-  console.log('connected', client.id)
-})
-
-// TODO handle upgrade to authenticate client
-
-const pingInterval: NodeJS.Timeout = setInterval((): void => {
-  wss.clients.forEach((ws: WebSocket) => {
-    const client = ws as MyWebSocket
-    if (!client.alive) {
-      client.terminate()
-      console.log('teminate', client.id)
-    }
-    client.alive = false
-    client.ping()
-    console.log('ping to ', client.id)
-  })
-}, PING_INTERVAL_MILLIS)
-wss.on('close', () => clearInterval(pingInterval))
+const wss = createWebSocketServer(server)
 
 server.listen(8080)
