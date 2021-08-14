952f96942aecde7400ad55440f7fdd7e17c8243c
Hiroki Itoh <ihiroky@gmail.com>
2021-08-14 19:02:48 +0900

    refactor: Use hooks at comment/App

--- packages/comment/src/index.tsx
+++ packages/comment/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import { App } from './App';
 import { LoginForm } from './LoginForm'
 import * as serviceWorker from './serviceWorker';
 
