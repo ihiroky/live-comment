d743b75cbd3ada4cab5d1a4debf35c72fffc0f77
Hiroki Itoh <ihiroky@gmail.com>
2022-07-23 01:02:26 +0900

    fix: Introduce aggressive mode
    Display comments at the same time a tab opens

--- packages/app/src/extension/background.ts
+++ packages/app/src/extension/background.ts
@@ -4,6 +4,31 @@ import { TargetTab } from './types'
 
 const log = getLogger('background')
 
+function canShowComments(tabId: number): boolean {
+  // Can't show comments if log window is not open.
+  log.info(store.cache.logTab.tabId, !store.cache.logTab.tabId)
+  if (!store.cache.logTab.tabId) {
+    return false
+  }
+
+  // Can show comments if show switch is ON in non agressive mode.
+  const tabIds = store.cache.showCommentTabs.tabIds
+  const ok = tabIds[tabId]
+  if (!store.cache.aggressive) {
+    return ok
+  }
+
+  // Show comments forcibly in agressive mode.
+  if (!ok) {
+    const newTabIds: Record<number, true> = {
+      ...tabIds,
+      [tabId]: true,
+    }
+    store.update('showCommentTabs', { tabIds: newTabIds })
+  }
+  return true
+}
+
 function addMessageListener(): void {
   chrome.runtime.onMessage.addListener((message: TargetTab, sender: chrome.runtime.MessageSender): void => {
     if (message.type !== 'target-tab') {
@@ -14,7 +39,7 @@ function addMessageListener(): void {
       return
     }
 
-    const status = store.cache['lc.shown-comemnts-tab'].tabIds[tabId] ? 'added' : undefined
+    const status = canShowComments(tabId) ? 'added' : undefined
     const response: TargetTab = {
       type: 'target-tab',
       status,
